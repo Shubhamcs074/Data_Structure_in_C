29772b9a8b7fd7ba49d3a1c34e1524b0d2a7130d
Shubhamcs074 <shubhamsaini1027bagar@gmail.com>
2025-07-09 18:59:06 +0530

    Updated Stack implementation using linked list

--- StackInLinkedList.c
+++ StackInLinkedList.c
@@ -1,73 +1,85 @@
-// C program to create a stack with given capacity
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Stack {
-    int top, cap;
-    int *a;
-};
-
-struct Stack* createStack(int cap) {
-    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-    stack->cap = cap;
-    stack->top = -1;
-    stack->a = (int*)malloc(cap * sizeof(int));
-    return stack;
+typedef struct Stack {
+    int data;
+    struct Stack* next;
+} stack ;
+stack* top = 0;
+void push(int value) {
+    stack* newNode;
+    newNode = (stack*)malloc(sizeof(stack));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
 }
-
-void deleteStack(struct Stack* stack) {
-    free(stack->a);
-    free(stack);
-}
-
-int isFull(struct Stack* stack) {
-    return stack->top >= stack->cap - 1;
-}
-
-int isEmpty(struct Stack* stack) {
-    return stack->top < 0;
-}
-
-int push(struct Stack* stack, int x) {
-    if (isFull(stack)) {
-        printf("Stack Overflow\n");
-        return 0;
+void display() {
+    stack* temp = top;
+    if(top == 0) {
+        printf("Stack is empty\n");
+        return;
     }
-    stack->a[++stack->top] = x;
-    return 1;
+    else{
+        printf("Stack elements are: ");
+        while(temp != 0) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+    }
+    printf("\n");
 }
 
-int pop(struct Stack* stack) {
-    if (isEmpty(stack)) {
+void peek() {
+    if( top == 0) {
+        printf("Stack is empty\n");
+        return;
+    }
+    else {
+        printf("Top element is %d\n", top->data);
+    }
+}
+    
+void pop() {
+    stack* temp = top;
+    if(top == 0) {
         printf("Stack Underflow\n");
-        return 0;
+        return;
+    }else {
+        top = top->next;
+        printf("Popped %d from the stack\n", temp->data);
+        free(temp);
     }
-    return stack->a[stack->top--];
 }
 
-int peek(struct Stack* stack) {
-    if (isEmpty(stack)) {
-        printf("Stack is Empty\n");
-        return 0;
-    }
-    return stack->a[stack->top];
-}
+int main () {
+    int choice, value;
+    do{
+    printf("Stack using Linked List\n");
+    printf("Choose a option :\n");
+    printf("1. Push\n2. Pop\n3. Peek\n4. Display\n5. Exit");
+    scanf("%d",&choice);
+    switch (choice) {
+        case 1:
+            printf("Enter value to push: ");
+            scanf("%d", &value);
+            push(value);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peek();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+     }
+     }while(choice != 5);
 
-int main() {
-    struct Stack* s = createStack(5);
-    push(s, 10);
-    push(s, 20);
-    push(s, 30);
-    printf("%d popped from stack\n", pop(s));
-
-    printf("Top element is: %d\n", peek(s));
-
-    printf("Elements present in stack: ");
-    while (!isEmpty(s)) {
-        printf("%d ", peek(s));
-        pop(s);
-    }
-
-    deleteStack(s);
-    return 0;
+return 0;
 }
